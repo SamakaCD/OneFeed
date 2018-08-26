b5b646c822fc82ace731e347089804a292a1da87
Ivan Sadovyi <samakacd@gmail.com>
2018-08-26 17:58:18 +0300

    #5: Bind view of FeedActivity directly to FeedItemsStore

--- app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedViewModel.kt
+++ app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedViewModel.kt
@@ -1,37 +1,25 @@
 package com.ivansadovyi.onefeed.presentation.screens.feed
 
 import android.databinding.BaseObservable
-import android.databinding.Bindable
 import com.ivansadovyi.domain.feed.FeedItemsStore
-import com.ivansadovyi.onefeed.BR
-import com.ivansadovyi.onefeed.presentation.utils.ObservableField
-import com.ivansadovyi.sdk.FeedItem
 import javax.inject.Inject
 
-class FeedViewModel @Inject constructor(private val feedItemsStore: FeedItemsStore) : BaseObservable() {
-
-	@get:Bindable
-	var items by ObservableField(fieldId = BR.items, value = emptyList<FeedItem>())
-
-	@get:Bindable
-	var loading by ObservableField(fieldId = BR.loading, value = false)
+class FeedViewModel @Inject constructor(val feedItemsStore: FeedItemsStore) : BaseObservable() {
 
 	init {
 		bindStore()
 	}
 
-	@Bindable("loading")
 	fun getFeedState(): FeedState {
 		return when {
-			loading -> FeedState.LOADING
+			feedItemsStore.loading -> FeedState.LOADING
 			else -> FeedState.READY
 		}
 	}
 
 	private fun bindStore() {
 		feedItemsStore.observable.subscribe {
-			items = it.items
-			loading = it.loading
+			notifyChange()
 		}
 	}
 }
