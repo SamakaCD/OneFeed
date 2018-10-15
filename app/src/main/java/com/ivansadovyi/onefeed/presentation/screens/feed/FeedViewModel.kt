926278b7a9f754eb5b7712e7998167b2dc529317
DESKTOP-8Q6VC6O\Ivan <samakacd@gmail.com>
2018-10-15 12:08:24 +0300

    #9: Feed: modify FeedRecyclerViewAdapter according to delegates support

--- app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedViewModel.kt
+++ app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedViewModel.kt
@@ -3,9 +3,10 @@ package com.ivansadovyi.onefeed.presentation.screens.feed
 import android.databinding.BaseObservable
 import android.databinding.Bindable
 import com.ivansadovyi.domain.feed.FeedItemsStore
+import java.util.*
 import javax.inject.Inject
 
-class FeedViewModel @Inject constructor(@Bindable val feedItemsStore: FeedItemsStore) : BaseObservable() {
+class FeedViewModel @Inject constructor(private val feedItemsStore: FeedItemsStore) : BaseObservable() {
 
 	init {
 		bindStore()
@@ -18,6 +19,17 @@ class FeedViewModel @Inject constructor(@Bindable val feedItemsStore: FeedItemsS
 			feedItemsStore.loading && feedItemsStore.items.isEmpty() -> FeedState.LOADING
 			else -> FeedState.READY
 		}
+	}
+
+	@Bindable
+	fun getItems(): List<Any> {
+		val items = ArrayList<Any>(feedItemsStore.items)
+
+		if (feedItemsStore.loading && feedItemsStore.items.isNotEmpty()) {
+			items.add(PaginationLoadingItem())
+		}
+
+		return items
 	}
 
 	fun loadMore() {
