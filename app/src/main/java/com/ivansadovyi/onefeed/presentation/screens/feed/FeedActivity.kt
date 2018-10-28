cc933fc3c058edb97b1347fdb455dcabd49ad0fa
Ivan Sadovyi <ivan.sadovyi@teamvoy.com>
2018-10-28 23:43:17 +0200

    Splash screen attempt number 1

--- app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedActivity.kt
+++ app/src/main/java/com/ivansadovyi/onefeed/presentation/screens/feed/FeedActivity.kt
@@ -1,17 +1,12 @@
 package com.ivansadovyi.onefeed.presentation.screens.feed
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.ivansadovyi.onefeed.R
-import com.ivansadovyi.onefeed.databinding.ActivityFeedBinding
-import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_feed.*
 import javax.inject.Inject
 
-class FeedActivity : AppCompatActivity() {
+class FeedActivity : Activity() {
 
 	@Inject
 	lateinit var viewModel: FeedViewModel
@@ -20,29 +15,30 @@ class FeedActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		AndroidInjection.inject(this)
-		val binding = DataBindingUtil.setContentView<ActivityFeedBinding>(this, R.layout.activity_feed)
-		binding.viewModel = viewModel
-		setupRecyclerView()
+		//setContentView(R.layout.activity_feed)
+//		AndroidInjection.inject(this)
+//		val binding = DataBindingUtil.setContentView<ActivityFeedBinding>(this, R.layout.activity_feed)
+//		binding.viewModel = viewModel
+//		setupRecyclerView()
 	}
 
-	private fun setupRecyclerView() {
-		layoutManager = LinearLayoutManager(this)
-		recyclerView.layoutManager = layoutManager
-		recyclerView.adapter = FeedRecyclerViewAdapter()
-		setupPagination()
-	}
-
-	private fun setupPagination() {
-		recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-				val visibleItemCount = layoutManager.childCount
-				val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
-				val totalItemCount = layoutManager.itemCount
-				if (lastVisibleItemPosition + visibleItemCount * 2 >= totalItemCount) {
-					viewModel.loadMore()
-				}
-			}
-		})
-	}
+//	private fun setupRecyclerView() {
+//		layoutManager = LinearLayoutManager(this)
+//		recyclerView.layoutManager = layoutManager
+//		recyclerView.adapter = FeedRecyclerViewAdapter()
+//		setupPagination()
+//	}
+//
+//	private fun setupPagination() {
+//		recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//				val visibleItemCount = layoutManager.childCount
+//				val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+//				val totalItemCount = layoutManager.itemCount
+//				if (lastVisibleItemPosition + visibleItemCount * 2 >= totalItemCount) {
+//					viewModel.loadMore()
+//				}
+//			}
+//		})
+//	}
 }
