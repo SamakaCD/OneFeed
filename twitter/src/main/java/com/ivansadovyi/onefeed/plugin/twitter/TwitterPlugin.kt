30a1c27a82b4ecbff2da675dfbfbffca882701b6
Ivan Sadovyi <ivan.sadovyi@teamvoy.com>
2019-01-06 13:12:49 +0200

    #11: Twitter plugin: fetch first 20 items of timeline

--- twitter/src/main/java/com/ivansadovyi/onefeed/plugin/twitter/TwitterPlugin.kt
+++ twitter/src/main/java/com/ivansadovyi/onefeed/plugin/twitter/TwitterPlugin.kt
@@ -3,16 +3,48 @@ package com.ivansadovyi.onefeed.plugin.twitter
 import com.ivansadovyi.sdk.FeedItem
 import com.ivansadovyi.sdk.OneFeedPlugin
 import com.ivansadovyi.sdk.OneFeedPluginDescriptor
+import com.ivansadovyi.sdk.OneFeedPluginParams
 import com.ivansadovyi.sdk.auth.AuthorizationHandler
+import com.ivansadovyi.sdk.auth.AuthorizationState
+import twitter4j.Twitter
+import twitter4j.TwitterFactory
+import twitter4j.conf.ConfigurationBuilder
 
 class TwitterPlugin : OneFeedPlugin() {
+
+	lateinit var twitter: Twitter
+
+	override fun onInit(params: OneFeedPluginParams) {
+		super.onInit(params)
+		instantiateTwitter()
+	}
+
+	override fun onAuthorizationStateChanged(newParams: OneFeedPluginParams) {
+		super.onAuthorizationStateChanged(newParams)
+		instantiateTwitter()
+	}
 
 	override fun getAuthorizationHandler(): AuthorizationHandler {
 		return TwitterAuthorizationHandler()
 	}
 
 	override fun loadNextItems(): Iterable<FeedItem> {
-		return emptyList()
+		return twitter.homeTimeline.map(TwitterStatusMapper::toFeedItem)
+	}
+
+	private fun instantiateTwitter() {
+		val configurationBuilder = ConfigurationBuilder()
+				.setOAuthConsumerKey(BuildConfig.CONSUMER_KEY)
+				.setOAuthConsumerSecret(BuildConfig.CONSUMER_SECRET)
+
+		if (authorizationState == AuthorizationState.AUTHORIZED) {
+			val authToken = AccessTokenSerializer.deserialize(params.authorization!!)
+			configurationBuilder.setOAuthAccessToken(authToken.token)
+			configurationBuilder.setOAuthAccessTokenSecret(authToken.tokenSecret)
+		}
+
+		val configuration = configurationBuilder.build()
+		twitter = TwitterFactory(configuration).instance
 	}
 
 	companion object {
