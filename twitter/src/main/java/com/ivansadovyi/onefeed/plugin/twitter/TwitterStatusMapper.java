038db85335c17bf5eceabd8075344532c34e2d73
Ivan Sadovyi <samakacd@gmail.com>
2019-02-07 11:38:39 +0200

    #28: Twitter plugin: add item images support

--- twitter/src/main/java/com/ivansadovyi/onefeed/plugin/twitter/TwitterStatusMapper.java
+++ twitter/src/main/java/com/ivansadovyi/onefeed/plugin/twitter/TwitterStatusMapper.java
@@ -1,10 +1,17 @@
 package com.ivansadovyi.onefeed.plugin.twitter;
 
+import com.ivansadovyi.sdk.FeedImage;
 import com.ivansadovyi.sdk.FeedItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import twitter4j.MediaEntity;
 import twitter4j.Status;
 
 public class TwitterStatusMapper {
+
+	private static final String MEDIA_TYPE_PHOTO = "photo";
 
 	public static FeedItem toFeedItem(Status status) {
 		return new FeedItem.Builder()
@@ -13,7 +20,23 @@ public class TwitterStatusMapper {
 				.setContent(status.getText())
 				.setPublicationDate(status.getCreatedAt())
 				.setAvatarImageUrl(status.getUser().getBiggerProfileImageURL())
+				.setImages(getFeedImagesOfStatus(status))
 				.build();
+	}
+
+	private static List<FeedImage> getFeedImagesOfStatus(Status status) {
+		List<FeedImage> images = new ArrayList<>();
+		MediaEntity[] mediaEntities = status.getMediaEntities();
+		for (MediaEntity mediaEntity : mediaEntities) {
+			if (mediaEntity.getType().equals(MEDIA_TYPE_PHOTO)) {
+				FeedImage image = new FeedImage.Builder()
+						.setId(String.valueOf(mediaEntity.getId()))
+						.setUrl(mediaEntity.getMediaURLHttps())
+						.build();
+				images.add(image);
+			}
+		}
+		return images;
 	}
 }
 
