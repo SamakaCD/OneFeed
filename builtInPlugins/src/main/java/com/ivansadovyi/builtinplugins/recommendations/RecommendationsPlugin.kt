a712b310d355c98173e1321505962afa78a68cf9
Ivan Sadovyi <samakacd@gmail.com>
2019-03-09 11:18:52 +0200

    #35: Recommendations plugin: show only not authorized plugins

--- builtInPlugins/src/main/java/com/ivansadovyi/builtinplugins/recommendations/RecommendationsPlugin.kt
+++ builtInPlugins/src/main/java/com/ivansadovyi/builtinplugins/recommendations/RecommendationsPlugin.kt
@@ -1,8 +1,6 @@
 package com.ivansadovyi.builtinplugins.recommendations
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.core.content.ContextCompat
 import com.ivansadovyi.builtinplugins.R
 import com.ivansadovyi.onefeed.plugin.twitter.TwitterPlugin
 import com.ivansadovyi.sdk.FeedItem
@@ -14,6 +12,7 @@ import java.util.Collections.emptyList
 class RecommendationsPlugin : OneFeedPlugin(), OneFeedPlugin.OnSubItemClickListener {
 
 	private var didShowRecommendations = false
+	private val pluginRecommendationSubItemFactory by lazy { PluginRecommendationSubItemFactory(context) }
 
 	override fun getIcon(): Bitmap {
 		return Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565)
@@ -24,18 +23,20 @@ class RecommendationsPlugin : OneFeedPlugin(), OneFeedPlugin.OnSubItemClickListe
 			return emptyList()
 		}
 
+		val authorizedPluginDescriptors = host.pluginManager.authorizedPluginDescriptors
+		val subItems = PLUGINS_FOR_RECOMMENDATION.filter { !authorizedPluginDescriptors.contains(it) }
+				.map(pluginRecommendationSubItemFactory::create)
+
+		if (subItems.isEmpty()) {
+			// Nothing to recommend
+			return emptyList()
+		}
+
 		val item = FeedItem.Builder()
 				.setId(ITEM_ID)
 				.setPriority(FeedItem.Priority.HIGH)
 				.setTitle(context.getString(R.string.recommendations_item_title))
-				.setSubItems(listOf(
-						SubItem.Builder()
-								.setId(TWITTER_ID)
-								.setTitle(context.getString(R.string.recommendations_sub_item_twitter))
-								.setIconColor(ContextCompat.getColor(context, R.color.colorTwitter))
-								.setIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_twitter))
-								.build()
-				))
+				.setSubItems(subItems)
 				.build()
 
 		return listOf(item)
@@ -50,11 +51,13 @@ class RecommendationsPlugin : OneFeedPlugin(), OneFeedPlugin.OnSubItemClickListe
 	companion object {
 
 		private const val ITEM_ID = "1afec80b-2315-49db-97c7-cbe12f31921b"
-		private const val TWITTER_ID = "42324058-fa4b-473b-8ce8-04e58c5cccc9"
+		const val TWITTER_ID = "42324058-fa4b-473b-8ce8-04e58c5cccc9"
 
 		val DESCRIPTOR = OneFeedPluginDescriptor.Builder()
 				.setName("Plugin recommendations")
 				.setClassName(RecommendationsPlugin::class.java.name)
 				.build()
+
+		private val PLUGINS_FOR_RECOMMENDATION = listOf(TwitterPlugin.DESCRIPTOR)
 	}
 }
